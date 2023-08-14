8d683dc45ddfa9ccaf88ab0274acc7bbc22a6b4c
hwang-sunha <sunna96@naver.com>
2023-08-14 12:12:36 +0900

    ..

--- src/main/java/com/example/zapiertest/TestController.java
+++ src/main/java/com/example/zapiertest/TestController.java
@@ -3,11 +3,23 @@ package com.example.zapiertest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
     @GetMapping("welcome")
     public String welcome(){
         System.out.println("welcome 이 호출되었습니다.");
         return "welcome";
+    }
+    @GetMapping("list")
+    public  List<Integer>  List(){
+        System.out.println("list  가호출되었습니다.");
+        return List.of(1, 2, 3, 4, 5);
+
+    }
+    static class Data {
+        String name;
+        String key;
     }
 }
